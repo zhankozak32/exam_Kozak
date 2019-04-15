457afc58542073f8357dea4b89b7aa5786c4e5d7
Zhan Kozak <zhankozak@gmail.com>
2019-04-15 18:42:46 +0300

    final code

--- mario.cpp
+++ mario.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-void buildStairs(int height, int choice);
-void buildPyramide(int height, int choice2);
-void Show(int &height, int &choice);
-int main()
+void buildStairs(int height, int choice); //Функция которая дулает не полную перамиду
+void buildPyramide(int height, int choice2); //Функция на полную пирамиду
+void Show(int &height, int &choice); // Финкция которая виводит пирамиди
+int main() // Основной код
 {
-
+int height, choice;
+cout << "Enter height:";
+while (!(cin >> height) || (cin.peek() != '\n'))
+{
+cin.clear();
+while (cin.get() != '\n');
+cout << "Input error! Retry input" << endl;
+}
+cout << "Enter choice:";
+while (!(cin >> choice) || (cin.peek() != '\n'))
+{
+cin.clear();
+while (cin.get() != '\n');
+cout << "Input error! Retry input" << endl;
+}
+switch (choice)
+{
+case 1:
+buildStairs(height, choice);
+break;
+case 2:
+buildPyramide(height, choice);
+break;
+default:
+break;
+}
 system("pause");
 return 0;
 }
@@ -21,6 +46,7 @@ cout << "*";
 cout << endl;
 }
 }
+
 void buildPyramide(int height, int choice2)
 {
 for (int i = 0; i < height; i++)
