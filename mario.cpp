4261fba996b04448d4741dbb234aff0f1167009f
Zhan Kozak <zhankozak@gmail.com>
2019-04-15 18:41:28 +0300

    add funcs

--- mario.cpp
+++ mario.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
+void buildStairs(int height, int choice);
+void buildPyramide(int height, int choice2);
+void Show(int &height, int &choice);
 int main()
 {
 
 system("pause");
 return 0;
+}
+void buildStairs(int height, int choice)
+{
+for (int i = 0; i < height; i++)
+{
+for (int j = i; j < height - 1; j++)
+cout << " ";
+for (int j = 0; j <= i; j++)
+cout << "*";
+cout << endl;
+}
+}
+void buildPyramide(int height, int choice2)
+{
+for (int i = 0; i < height; i++)
+{
+for (int j = i; j < height - 1; j++)
+cout << " ";
+for (int j = 0; j <= i; j++)
+cout << "*";
+cout << " ";
+for (int j = 0; j <= i; j++)
+cout << "*";
+for (int j = i; j < height - 1; j++)
+cout << " ";
+cout << endl;
+}
+}
+void Show(int &height, int &choice)
+{
+cout << "Enter height:";
+cin >> height;
+cout << "Choose 1 or 2 :";
+cin >> choice;
+cout << endl;
 }
