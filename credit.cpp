38a460579108a1f643ba4a10f7ddc084d250a3c4
“Edward <edwardmelnyk1234ua@gmail.com>
2019-04-24 20:02:04 +0300

    added two funcs

--- credit.cpp
+++ credit.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void AlgorithmLuhn(long long& number);
+bool IsCorrect(const long long& number);
+
 int main()
 {
 
 system("pause");
 return 0;
+}
+
+void AlgorithmLuhn(long long& number)
+{
+int* digits = FormArr(number);
+
+int sum = 0;
+sum += SumEven(digits);
+sum += SumOdd(digits);
+
+IsValid(sum);
+}
+
+bool IsCorrect(const long long& number)
+{
+if (number / 10E14 < 1 || number / 10E14 > 9 || !cin)
+{
+cin.clear();
+while (cin.get() != '\n')
+continue;
+return false;
+}
+
+return true;
 }
