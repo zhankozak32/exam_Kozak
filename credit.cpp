03ebf41b5af7ad549f135cceb6fe2ef721a0f17b
Zhan Kozak <zhankozak@gmail.com>
2019-04-24 20:02:25 +0300

    added two funcs

--- credit.cpp
+++ credit.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int * FormArr(const long long& number);
+int SumOdd(int* arr);
+
 int main()
 {
 
 system("pause");
 return 0;
+}
+int * FormArr(const long long& number)
+{
+int* digits = new int[16];
+long long temp = number;
+
+for (int i = 0; i < 16; i++, temp /= 10)
+digits[i] = temp % 10;
+
+return digits;
+}
+
+int SumOdd(int * arr)
+{
+int sum = 0;
+for (int i = 0; i < 16; i += 2)
+sum += arr[i];
+
+return sum;
 }
